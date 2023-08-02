5518734396210127a7e5bafd38ca4b1c06d3b967
redfoxbg <redfoxbg@gmail.com>
2023-08-03 02:00:02 +0300

    Add Character , Race and Class creation

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@
 
             //Strings:
             string gender;
+            string race;
+            string classCh;
 
+            #region Character Cration
             do
             {
                 Console.WriteLine("Place choose a gender as below: ");
@@ -17,16 +20,65 @@
 
                 gender = Console.ReadLine().ToUpper();
 
-                if (gender == "MALE") correct = 1;
-
-                if (gender == "FEMALE") correct = 1;
+                if (gender == "FEMALE" || gender == "MALE") correct = 1;
                 else
                 {
 
                 }
 
             } while (correct == 0);
-            
+            correct = 0;
+
+            #endregion
+
+
+            #region Race Creation:
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Place choose a race as below: ");
+                Console.WriteLine("Human");
+                Console.WriteLine("Dwarf");
+                Console.WriteLine("Elf");
+                Console.WriteLine("Orc");
+                Console.WriteLine("Your Choice: ");
+
+                race = Console.ReadLine().ToUpper();
+
+                if (race == "HUMAN" || race == "DWARF" || race == "ELF" || race == "ORC") correct = 1;
+                else
+                {
+
+                }
+            }while (correct == 0);
+            correct = 0;
+
+            #endregion
+
+            #region Class Creation:
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Place choose a class as below: ");
+                Console.WriteLine("Warrior");
+                Console.WriteLine("Hunter");
+                Console.WriteLine("Mage");
+                Console.WriteLine("Thief");
+                Console.WriteLine("Your Choice: ");
+
+                classCh = Console.ReadLine().ToUpper();
+
+                if (classCh == "HUMAN" || classCh == "DWARF" || classCh == "ELF" || classCh == "ORC") correct = 1;
+                else
+                {
+
+                }
+            } while(correct == 0);
+            correct = 0;
+
+            #endregion
+
+
         }
     }
 }
