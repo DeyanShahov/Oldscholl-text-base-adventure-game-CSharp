08c69f4001696530dcae40ec01ce465aff6f1c34
redfoxbg <redfoxbg@gmail.com>
2023-08-03 01:14:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Oldscholl_text_base_adventure_game_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ints:
+            int correct = 0;
+
+            //Strings:
+            string gender;
+
+            do
+            {
+                Console.WriteLine("Place choose a gender as below: ");
+                Console.WriteLine("Male / Female");
+
+                gender = Console.ReadLine().ToUpper();
+
+                if (gender == "MALE") correct = 1;
+
+                if (gender == "FEMALE") correct = 1;
+                else
+                {
+
+                }
+
+            } while (correct == 0);
+            
+        }
+    }
+}
